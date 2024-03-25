940e288929de87cf334dcbc647c91b58168a0b71
Abraham Iglesias <abraham.iglesias@scrm.lidl>
2024-03-25 17:59:49 +0100

    first unit test

---
+++ src/new_feature/calculator.py
@@ -0,0 +1,14 @@
+class NotANumberException(Exception):
+    pass
+
+
+class Calculator:
+    def __init__(self):
+        print("hello this is the instance")
+
+    def sum(self, a, b):
+        if type(b) is not int:
+            raise NotANumberException()
+        return a + b
+
+
--- tests/units/test_hello_world.py
+++ tests/units/test_hello_world.py
@@ -1,2 +1,16 @@
-def test_hello_world():
-    assert True
+from hamcrest import assert_that, calling, raises
+
+from src.new_feature.calculator import Calculator, NotANumberException
+
+
+def test_successful_sum():
+    calculator = Calculator()
+    result = calculator.sum(2, 5)
+
+    assert result == 7
+
+
+def test_unsuccessful_sum():
+    calculator = Calculator()
+
+    assert_that(calling(calculator.sum).with_args(2, "patata"), raises(NotANumberException))
